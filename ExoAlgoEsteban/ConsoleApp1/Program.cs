b96b9aec540c6844c0804889e68135e8f2d7a7ab
Esteban-dev <74709808+Esteban-dev@users.noreply.github.com>
2021-06-03 16:15:19 +0200

    Division

---
+++ ExoAlgoEsteban/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Diviseur_de_Nombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("définir le nombre entier n");
+            int n = int.Parse(Console.ReadLine());
+            int x = 1;
+
+
+            while (x != n+1)
+            {
+                //code que je repete ds la boucle
+                int diviseur = n % x;
+                if (diviseur == 0)
+                {
+                    Console.WriteLine(x + " est un divsiseur de " + n);
+                }
+                x++;
+            }
+        }
+    }
+}
