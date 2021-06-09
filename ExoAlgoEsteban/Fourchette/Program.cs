6ab7fc07c7e48f2c6775657d31b4111e7859ba83
sthiry2 <sthiry@arfp.asso.fr>
2021-06-09 14:22:47 +0200

    ST Ajout boucle saisie

--- ExoAlgoEsteban/Fourchette/Program.cs
+++ ExoAlgoEsteban/Fourchette/Program.cs
@@ -21,7 +21,19 @@ namespace Fourchette
             nombre_ordinateur = new Random().Next(limite_inferieur, limite_superieur);
             Console.WriteLine("Bonjour dans ce  jeux de la fourchette l'ordinateur à pris un nombre entier naturel situé entre 0 et 100");
             Console.WriteLine(", a vous de trouver ce nombre en cherchant entre les limites supérieurs et inférieurs.");
-            nombre_propose_par_Joueur = int.Parse(Console.ReadLine());
+
+            //saisie du nombre de  l'utilisateur
+            do
+            {
+                nombre_propose_par_Joueur = int.Parse(Console.ReadLine());
+                if (nombre_propose_par_Joueur < limite_inferieur || nombre_propose_par_Joueur > limite_superieur)
+                {
+                    Console.WriteLine("Le nombre n'est pas dans l'intervalle " + limite_inferieur + " et " + limite_superieur+ " Veuillez me donner un nouveau nombre svp:");
+                }
+            }
+            while (nombre_propose_par_Joueur < limite_inferieur || nombre_propose_par_Joueur > limite_superieur);
+
+
 
 
 
